b9fc49576d7602a0b0b213e375a05f35fabf7095
Alexander-Cholmsky <acholmsky@gmail.com>
2021-06-13 17:50:01 -0400

    view to see schedule with kids

--- schedule/models.py
+++ schedule/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import datetime
 
 from phonenumber_field.modelfields import PhoneNumberField
 
@@ -10,19 +11,37 @@ class Parent(models.Model):
     email = models.CharField(max_length=200)
     phone_number = PhoneNumberField()
 
+    def __str__(self):
+        return self.first_name + self.last_name
+
 class Child(models.Model):
     parent = models.ForeignKey(Parent, on_delete= models.CASCADE)
     first_name = models.CharField(max_length=200, default='name')
     last_name = models.CharField(max_length=200, default='name')
     age = models.IntegerField()
 
+    def __str__(self):
+        return self.first_name
+
 class Week(models.Model):
-    child = models.ForeignKey(Child, on_delete = models.CASCADE)
+    child = models.ManyToManyField(Child)
     start_date = models.DateField()
     end_date = models.DateField()
     registered = models.BooleanField()
+
+    def __str__(self):
+        smonth = self.start_date.strftime("%m")
+        sday = self.start_date.strftime("%d")
+        emonth = self.end_date.strftime("%m")
+        eday = self.end_date.strftime("%d")
+
+        # converts dates to strings
+        return smonth + '/' + sday + '-' + emonth + '/' + eday
+
 
 class Day(models.Model):
     week = models.ForeignKey(Week, on_delete = models.CASCADE)
     date = models.DateField()
     itinerary = models.TextField()
+
+    
--- schedule/views.py
+++ schedule/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from django.template import loader
+
 from django.http import HttpResponse
+from .models import Week, Child
 
 # Create your views here.
 def schedule(request):
-    return HttpResponse("This is where you will find a schedule")
+    schedule = Week.objects.all()
+    context = {
+        'schedule': schedule,
+    
+    }
+    return render(request, 'schedule/calendar.html',context)
